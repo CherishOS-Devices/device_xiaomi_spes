71dd29c005bacdeb6a764f1ea123071f318d9143
sthomsonpl <tomekciupa07.02@wp.pl>
2022-06-06 16:54:43 +0200

    spes: perf:  switch to vendor.qti.hardware.perf

--- device.mk
+++ device.mk
@@ -390,7 +390,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Perf
 PRODUCT_PACKAGES += \
-    libqti-perfd-client
+    vendor.qti.hardware.perf@2.2 \
+    vendor.qti.hardware.perf@2.2.vendor
     
     PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
